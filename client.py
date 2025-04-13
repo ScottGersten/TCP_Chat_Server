27c2b88229439c0cb0fd64433bc349dcf1d2941b
ScottGersten <130173316+ScottGersten@users.noreply.github.com>
2025-04-13 15:05:29 -0400

    Added graceful exits and shutdown
    The clients can now disconnect with "/kill" and once all clients have disconnected the server shuts down. Also turned some hard coded values into constants for both classes.

--- client.py
+++ client.py
@@ -2,29 +2,38 @@ import socket
 import threading
 
 class Client:
-    def __init__(self, host="127.0.0.1", port=55555, buff_size=1024):
+    def __init__(self, host="127.0.0.1", port=55555, buff_size=1024,
+                 nick_name_msg="***NICK_NAME***", exit_msg = "/kill"):
         self.nickname = input("Choose a nickname: ")
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((host, port))
-        self.buff_size = buff_size
+        self.BUFFER_SIZE = buff_size
+        self.NICK_NAME_MESSAGE = nick_name_msg
+        self.EXIT_MESSAGE = exit_msg
 
     def receive(self):
         while True:
             try:
-                message = self.client.recv(self.buff_size).decode("ascii")
-                if message == "***NICK_NAME***":
+                message = self.client.recv(self.BUFFER_SIZE).decode("ascii")
+                if message == self.NICK_NAME_MESSAGE:
                     self.client.send(self.nickname.encode("ascii"))
                 else:
                     print(message)
             except:
-                print("An error occurred")
+                print("You have disconnected from the server.")
                 self.client.close()
                 break
     
     def write(self):
         while True:
-            message = f"{self.nickname}: {input("")}"
-            self.client.send(message.encode("ascii"))
+            #message = f"{self.nickname}: {input("")}"
+            message = input("")
+            if message == self.EXIT_MESSAGE:
+                self.client.send(self.EXIT_MESSAGE.encode("ascii"))
+                self.client.close()
+                break
+            else:
+                self.client.send(f"{self.nickname}: {message}".encode("ascii"))
 
     def run(self):
         receive_thread = threading.Thread(target=self.receive)
--- server.py
+++ server.py
@@ -2,14 +2,17 @@ import threading
 import socket
 
 class Server:
-    def __init__(self, host="127.0.0.1", port=55555, buffer_size=1024):
+    def __init__(self, host="127.0.0.1", port=55555, buffer_size=1024, 
+                 nick_name_msg="***NICK_NAME***", exit_msg="/kill"):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((host, port))
         self.server.listen()
         self.clients = []
         self.nicknames = []
-        self.buffer_size = buffer_size
-        print("The server is listening")
+        self.BUFFER_SIZE = buffer_size
+        self.NICK_NAME_MESSAGE = nick_name_msg
+        self.EXIT_MESSAGE = exit_msg
+        print("The server is listening...")
 
     def broadcast(self, msg):
         for client in self.clients:
@@ -18,33 +21,46 @@ class Server:
     def handle(self, client):
         while True:
             try:
-                message = client.recv(self.buffer_size)
+                message = client.recv(self.BUFFER_SIZE)
+                decoded_message = message.decode("ascii")
+                if decoded_message == self.EXIT_MESSAGE:
+                    raise Exception("Client requested disconnect.")
                 self.broadcast(message)
             except:
                 index = self.clients.index(client)
-                client.remove(self.clients)
-                client.close()
                 nickname = self.nicknames[index]
-                self.broadcast(f"{nickname} left the chat".encode("ascii"))
+                #client.remove(self.clients)
+                self.clients.remove(client)
                 self.nicknames.remove(nickname)
+                client.close()
+                self.broadcast(f"{nickname} left the chat.".encode("ascii"))
+                print(f"{nickname} has disconnected.")
+
+                if not self.clients:
+                    print("All clients have disconnected. Shutting down the server.")
+                    self.server.close()
+
                 break
 
     def receive(self):
         while True:
-            client, address = self.server.accept()
-            print(f"Connected with {str(address)}")
+            try:
+                client, address = self.server.accept()
+                print(f"Connected with {str(address)}.")
 
-            client.send("***NICK_NAME***".encode("ascii"))
-            nickname = client.recv(self.buffer_size).decode("ascii")
-            self.nicknames.append(nickname)
-            self.clients.append(client)
+                client.send(self.NICK_NAME_MESSAGE.encode("ascii"))
+                nickname = client.recv(self.BUFFER_SIZE).decode("ascii")
+                self.nicknames.append(nickname)
+                self.clients.append(client)
 
-            print(f"nickname of the client is {nickname}")
-            client.send("Welcome, you are connected to the server. \"/kill\" to exit".encode("ascii"))
-            self.broadcast(f"{nickname} has joined the chat".encode("ascii"))
+                print(f"nickname of the client is {nickname}")
+                client.send(f"Welcome, you are connected to the server. \"{self.EXIT_MESSAGE}\" to exit.".encode("ascii"))
+                self.broadcast(f"{nickname} has joined the chat.".encode("ascii"))
 
-            thread = threading.Thread(target=self.handle, args=(client,))
-            thread.start()
+                thread = threading.Thread(target=self.handle, args=(client,))
+                thread.start()
+            except OSError:
+                break
 
 if __name__ == "__main__":
     server = Server()
